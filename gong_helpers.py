bf1fe1260aa4799bfe5aa16081c3a33d21486ed3
David Stansby <dstansby@gmail.com>
2019-04-08 12:14:38 +0100

    Add gong_sync function

--- gong_helpers.py
+++ gong_helpers.py
@@ -4,7 +4,40 @@ import sunpy.io.fits
 
 import numpy as np
 
-map_dir = pathlib.Path('/Users/dstansby/data/gong/daily_synoptic')
+map_dir = pathlib.Path('/Users/dstansby/Data/gong/daily_synoptic')
+
+
+def sync_gong(year=datetime.now().year,
+              month=datetime.now().month):
+    from ftplib import FTP
+    import parfive
+    year = str(year)
+    month = f'{month:02}'
+
+    local = map_dir / (year + month)
+
+    ftp = FTP('gong2.nso.edu')
+    ftp.login()
+    ftp.cwd(f"/oQR/zqs/{year}{month}/")
+    daydirs = ftp.nlst()
+
+    # List to store files that need to be downloaded
+    dl = parfive.Downloader()
+    for daydir in daydirs:
+        ftp.cwd(f"/oQR/zqs/{year}{month}/{daydir}")
+        files = ftp.nlst()
+        for file in files:
+            local_file = local / daydir / file
+            if not local_file.exists():
+                dl.enqueue_file(f"ftp://gong2.nso.edu/oQR/zqs/{year}{month}/{daydir}/{file}", local / daydir, file)
+
+    if dl.queued_downloads > 0:
+        print(f'Downloading {dl.queued_downloads} files')
+        dl.download()
+
+
+def get_closest_map(dtime):
+    latest_gong_map()
 
 
 def gong_daily_files(year, month):
