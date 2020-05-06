a839e36059184a7e1f293dbb982cb727aa696499
David Stansby <dstansby@gmail.com>
2020-05-06 16:55:48 +0100

    Manually set start/end times

--- main.py
+++ main.py
@@ -84,19 +84,11 @@ def create_figure(dtime, aia_map):
 
 
 if __name__ == '__main__':
-    # Get perihelion number from the command line
-    parser = argparse.ArgumentParser()
-    parser.add_argument('peri_n', metavar='n', type=int,
-                        help='perihelion number')
-    args = parser.parse_args()
-    peri_n = args.peri_n
-
-    # Read in list of perihelion dates
-    peri_dates = pd.read_csv(
-        'peri_dates.csv', parse_dates=[1, 2]).set_index('n')
-    peri_dates = peri_dates.loc[peri_n]
-    sdate = peri_dates['start']
-    edate = peri_dates['end']
+    # Set start date and end date
+    #
+    # For PSP, see peri_dates.csv for a list
+    sdate = datetime(2020, 5, 1)
+    edate = datetime.now() + timedelta(days=7)
     print(sdate, edate)
 
     # Get an AIA synoptic map
